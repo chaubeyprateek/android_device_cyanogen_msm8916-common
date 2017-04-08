966399e2268f99a7c109cf1888a9ad173d5015ad
karthick111 <karthickrko61@gmail.com>
2017-04-08 05:29:36 +0200

    msm8916-common: Add missed out props
    https://github.com/LineageOS/android_device_cyanogen_msm8939-common/blob/cm-12.1/system.prop#L80
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- product/perf.mk
+++ product/perf.mk
@@ -1,9 +1,15 @@
 # Properties
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=4
-endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
     ro.config.max_starting_bg=8
+
+# Trim
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.use_trim_settings=true \
+    ro.sys.fw.empty_app_percent=50 \
+    ro.sys.fw.trim_empty_percent=100 \
+    ro.sys.fw.trim_cache_percent=100 \
+    ro.sys.fw.trim_enable_memory=1073741824
